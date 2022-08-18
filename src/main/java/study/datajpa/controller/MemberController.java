49888cd95efa12514ce8c4501c77bb31ad47d435
LEEYOSEPH <laksmi0628@gmail.com>
2022-08-19 08:16:11 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,6 +1,8 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,12 @@ public class MemberController {
     public String findMember2(@PathVariable("id") Member member) {
 
         return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
     }
 
 
