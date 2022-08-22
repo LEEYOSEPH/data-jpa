2da61f2ad7c2847b90bade3c36c9364b8febc1e3
LEEYOSEPH <laksmi0628@gmail.com>
2022-08-23 07:53:14 +0900

    네이티브 쿼리

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -62,4 +62,7 @@ public interface MemberRepository extends JpaRepository<Member,Long>,MemberRepos
     List<Member> findByUsernamess(String name);
 
     <T> List<T> findProjectionsByUsername(String username, Class<T> type);
+
+    @Query(value = "select * from member where username = ?", nativeQuery = true)
+    Member findByNativeQuery(String username);
 }
